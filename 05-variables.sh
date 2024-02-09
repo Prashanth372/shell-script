ba82e0eed3fcc4af7642885e06b0f53720c31306
Prashanth <mprashanth372@gmail.com>
2024-02-09 22:41:46 +0530

    adding more data into shell script file

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME #this is the variabale weher the value of one user entered
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+
+read PASSWORD
+
+echo "Password entered is : $PASSWORD"
