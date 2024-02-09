81b4a9820ab6b2059c877bcb6326c18d24be4716
Prashanth <mprashanth372@gmail.com>
2024-02-09 22:25:37 +0530

    adding more data into shell script file

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1 # this is the first value from command line
+PERSON2=$2 # this is the second value from command line
+
+echo "Hello world i am learning Devops with AWS cloud"
+
+echo "$PERSON1: Hi $PERSON2, How are you doing?"
+echo "$PERSON2: I am good what about you $PERSON1?"
+echo "$PERSON1: I am also feeling better than yesterday $PERSON2."
