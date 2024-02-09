41de332d03a19624cbbea99aeeee4be672d7cdd3
Prashanth <mprashanth372@gmail.com>
2024-02-09 15:06:14 +0530

    adding more data into shell script file

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PERSON1=RAMESH
-PERSON2=SURESH
+PERSON1=Sachin  
+PERSON2=Tendulkar
 
 echo "Hello world i am learning Devops with AWS cloud"
 
 echo "$PERSON1: Hi $PERSON2, How are you doing?"
 echo "$PERSON2: I am good what about you $PERSON1?"
-echo "$PERSON1: I am also feeling better than yesterday $PERSON1."
+echo "$PERSON1: I am also feeling better than yesterday $PERSON2."
