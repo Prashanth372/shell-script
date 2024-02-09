08c88a181ac3620a1dcae41d7ce822afb8365441
Prashanth <mprashanth372@gmail.com>
2024-02-09 09:38:31 +0530

    adding content to it

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "Hello world i am learning Devops with AWS cloud"
+
+echo "Ramesh: Hi Suresh & Mahesh, How are you doing?"
+echo "Suresh: I am good what about you?"
+echo "Mahesh: I am also feeling better than yesterday."
