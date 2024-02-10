72a514688854b66f6bb1f91cfbdcb7906de5c526
Prashanth <mprashanth372@gmail.com>
2024-02-10 19:15:42 +0530

    adding conditions file

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -7,8 +7,8 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: Please run this script with root access"
-else    
-    # echo "INFO:: You are root user"
+# else    
+#   echo "INFO:: You are root user"
 fi
 
 yum install git -y
