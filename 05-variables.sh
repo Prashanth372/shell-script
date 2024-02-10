72bf27593eef21bc5358a389496ecf7e132e4cd7
Prashanth <mprashanth372@gmail.com>
2024-02-10 16:21:45 +0530

    adding more data into shell script file

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,7 @@ echo "Please enter your username"
 
 read -s USERNAME #this is the variabale weher the value of one user entered
 
+#this is only for validation and learning purpose and should not print in terminal
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
---
+++ 06-array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#this is called as array , insted if single value it holds multiple values
+PERSONS=("Ramesh" "Suresh" "Sachin")
+#inside arrray we always refer values with index and it starts from 0
+echo "First person : ${PERSONS[0]}"
