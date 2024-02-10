c8e1141edbf6988031ff2fec385bfe375ecc5493
Prashanth <mprashanth372@gmail.com>
2024-02-11 00:37:56 +0530

    adding conditions file

--- 09-functions.sh
+++ 09-functions.sh
@@ -8,10 +8,10 @@ VALIDATE(){
     #$1--->it will receive the argument1
     if [ $1 -ne 0 ]
     then    
-        echo "Installation ... FAILURE"
+        echo "$2 ... FAILURE"
         exit 1
     else 
-        echo "Installation ... SUCCESS"
+        echo "$2 ... SUCCESS"
     fi
 }
 
@@ -25,11 +25,11 @@ fi
 
 yum install mysql -y
 
-VALIDATE $?
+VALIDATE $? "Installing MYSQL"
 
 
 yum install postfix -y
 
-VALIDATE $?
+VALIDATE $? "Installing Postfix"
 
 
