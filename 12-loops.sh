fb337f81bb7d2af6824e8df01f4a89bf43efd5d5
Prashanth <mprashanth372@gmail.com>
2024-02-12 20:46:08 +0530

    adding conditions file

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+if [ $USERID ne- 0 ]
+then    
+    echo -e "$R ERROR:: Please run this scirpt with root access $N"
+    exit 1
+fi
+
+
 #all args are in $@
 
 for i in $@
@@ -7,9 +17,9 @@ do
     yum install $i -y
 done
 
-#improvement
-#implement log files
-#implement validations
-#implement validations through functions
-#your script should check package is already installed or not, if already installed print in yellow colour
-#if installed just print package is already installed,it should not run install command
+# improvement
+# implement log files
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print in yellow colour
+# if installed just print package is already installed,it should not run install command
