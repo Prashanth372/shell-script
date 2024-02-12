43cd9dd0402c279f629a425f328a8c066b6f90e4
Prashanth <mprashanth372@gmail.com>
2024-02-13 00:53:23 +0530

    adding conditions file

--- 12-loops.sh
+++ 12-loops.sh
@@ -10,7 +10,8 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-if [ $USERID -ne 0 ]
+
+if [ $USERID -ne 0 ];
 then    
     echo -e "$R ERROR:: Please run this scirpt with root access $N"
     exit 1
