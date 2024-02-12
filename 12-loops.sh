10b9b892a207cc1aa8ace40eb6219c9350cc3e78
Prashanth <mprashanth372@gmail.com>
2024-02-13 00:43:11 +0530

    adding conditions file

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
-
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
+SCRIPT_NAME=$0
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 if [ $USERID -ne 0 ]
 then    
     echo -e "$R ERROR:: Please run this scirpt with root access $N"
     exit 1
 fi
 
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}    
+
+
 
 #all args are in $@
 
 for i in $@
 do 
-    yum install $i -y
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then    
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo "$Y $i is already installed $N"
+    fi
+
+    #yum install $i -y
 done
 
 # improvement
